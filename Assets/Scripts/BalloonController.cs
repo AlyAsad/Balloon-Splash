fef946ddd55ef275255828da99444ec8e5148454
affanahmed2 <affanahmad323@gmail.com>
2024-07-17 14:06:14 +0300

    slingshot mechanic added

---
+++ Assets/Scripts/BalloonController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonController : MonoBehaviour
+{
+    [SerializeField] float power = 10f;
+    [SerializeField] float maxDrag = 5f;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] LineRenderer lr;
+
+    Vector3 dragStartPos;
+    Touch touch;
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                DragStart();
+            }
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Dragging();
+            }
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                DragRelease();
+            }
+
+        }
+    }
+
+    void DragStart()
+    {
+        dragStartPos = Camera.main.ScreenToWorldPoint(touch.position);
+        dragStartPos.z = 0f;
+        lr.positionCount = 1;
+        lr.SetPosition(0, dragStartPos);
+    }
+    void Dragging()
+    {
+        Vector3 draggingPos = Camera.main.ScreenToWorldPoint(touch.position);
+        draggingPos.z = 0f;
+        lr.positionCount = 2;
+        lr.SetPosition(1, draggingPos);
+    }
+    void DragRelease()
+    {
+        lr.positionCount = 0;
+
+        Vector3 dragReleasePos = Camera.main.ScreenToWorldPoint(touch.position);
+        dragReleasePos.z = 0f;
+
+        Vector3 force = dragStartPos - dragReleasePos;
+        Vector3 clampedForce = Vector3.ClampMagnitude(force, maxDrag) * power;
+
+        rb.AddForce(clampedForce, ForceMode2D.Impulse);
+    }
+
+}
