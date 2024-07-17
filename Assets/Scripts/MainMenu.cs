636212439cf289969480c774e56f2885e6eac112
Aly Asad Gilani <alyasad02@gmail.com>
2024-07-17 14:19:57 +0500

    LEVEL PROGRESS
    Fully implemented level progress functionality, and added a debug button in main menu to reset the level progress back to level 1.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,9 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    int currLevel;
+
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync("Level 1");
+        currLevel = PlayerPrefs.GetInt("Unlocked Level", 1);
+        switch (currLevel) {
+
+            case 1:
+                SceneManager.LoadSceneAsync("Level 1");
+                break;
+            
+            case 2:
+                SceneManager.LoadSceneAsync("Level 2");
+                break;
+            
+            default:
+                SceneManager.LoadSceneAsync("Level " + (currLevel - 1));
+                break;
+
+        }
     }
 
     public void LevelSelect()
@@ -18,6 +35,12 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ResetLevelProgress()
+    {
+        PlayerPrefs.SetInt("Unlocked Level", 1);
+        PlayerPrefs.Save();
     }
 }
 
