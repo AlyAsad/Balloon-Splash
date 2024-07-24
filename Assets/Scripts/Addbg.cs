de2acf981120c399c5e05a7c75e00ec04d0fd5af
Aly Asad Gilani <alyasad02@gmail.com>
2024-07-24 12:31:57 +0500

    Levels 2 and 3

--- Assets/Scripts/Addbg.cs
+++ Assets/Scripts/Addbg.cs
@@ -1,7 +1,7 @@
 using UnityEditor;
 using UnityEngine;
-using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
 
 public class AddBackgroundToAllScenes : EditorWindow
 {
