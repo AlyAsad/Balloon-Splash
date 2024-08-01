a05a7c9f41c3af4c8fd8fbee283dac537d2c3379
affanahmed2 <affanahmad323@gmail.com>
2024-08-01 17:00:23 +0300

    player health bar

--- Assets/Scripts/PlayerHealthBar.cs
+++ Assets/Scripts/PlayerHealthBar.cs
@@ -6,6 +6,16 @@ using UnityEngine.UI;
 public class PlayerHealthBar : MonoBehaviour
 {
     [SerializeField] Slider slider;
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offset;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        transform.position = target.position + offset;
+    }
 
     public void UpdateHealthBar(float currentValue, float maxValue)
     {
