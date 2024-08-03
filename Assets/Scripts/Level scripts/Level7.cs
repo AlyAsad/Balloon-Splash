2a362cdc4cd2c0cf26625b6201c53e7f18e29b33
Aly Asad Gilani <alyasad02@gmail.com>
2024-08-03 15:48:55 +0500

    Scripts

--- Assets/Scripts/Level scripts/Level7.cs
+++ Assets/Scripts/Level scripts/Level7.cs
@@ -5,16 +5,54 @@ using UnityEngine.SceneManagement;
 
 public class Level7 : MonoBehaviour
 {
-    public void BackToMenu()
+
+    [SerializeField] private GameObject LevelCompleteUI;
+    [SerializeField] private GameObject LevelFailedUI;
+    [SerializeField] private GameObject enemy1;
+    [SerializeField] private GameObject enemy2;
+    [SerializeField] private GameObject player;
+
+    private bool lvlPassed = false, lvlFailed = false;
+
+
+    private void Update()
     {
-        SceneManager.LoadSceneAsync("MainMenu");
+        if (lvlPassed || lvlFailed) return;
+
+        // if enemy died, level passed
+        if (enemy1 == null && enemy2 == null) {
+            lvlPassed = true;
+            levelPassed();
+        }
+
+        // if player died, level failed
+        else if (player == null) {
+            lvlFailed = true;
+            levelFailed();
+        }
+
     }
+
 
     public void levelPassed() 
     {   
+        LevelCompleteUI.SetActive(true);
+        Time.timeScale = 0f;
+        PauseMenu.gameIsPaused = true;
+
         if (PlayerPrefs.GetInt("Unlocked Level", 1) == 7) {
             PlayerPrefs.SetInt("Unlocked Level", 8);
             PlayerPrefs.Save();
         }
     }
+
+
+    public void levelFailed()
+    {
+        LevelFailedUI.SetActive(true);
+        Time.timeScale = 0f;
+        PauseMenu.gameIsPaused = true;
+    }
+
+
 }
