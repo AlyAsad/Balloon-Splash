38ad1fbdcbb5bef29d7f53fc6841378f340505a0
Aly Asad Gilani <alyasad02@gmail.com>
2024-08-03 14:40:55 +0500

    Added proper UI to level 2

--- Assets/Scripts/Level scripts/Level2.cs
+++ Assets/Scripts/Level scripts/Level2.cs
@@ -5,16 +5,53 @@ using UnityEngine.SceneManagement;
 
 public class Level2 : MonoBehaviour
 {
-    public void BackToMenu()
+
+    [SerializeField] private GameObject LevelCompleteUI;
+    [SerializeField] private GameObject LevelFailedUI;
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private GameObject player;
+
+    private bool lvlPassed = false, lvlFailed = false;
+
+
+    private void Update()
     {
-        SceneManager.LoadSceneAsync("MainMenu");
+        if (lvlPassed || lvlFailed) return;
+
+        // if enemy died, level passed
+        if (enemy == null) {
+            lvlPassed = true;
+            levelPassed();
+        }
+
+        // if player died, level failed
+        else if (player == null) {
+            lvlFailed = true;
+            levelFailed();
+        }
+
     }
+
 
     public void levelPassed() 
     {   
+        LevelCompleteUI.SetActive(true);
+        Time.timeScale = 0f;
+        PauseMenu.gameIsPaused = true;
+
         if (PlayerPrefs.GetInt("Unlocked Level", 1) == 2) {
             PlayerPrefs.SetInt("Unlocked Level", 3);
             PlayerPrefs.Save();
         }
     }
+
+
+    public void levelFailed()
+    {
+        LevelFailedUI.SetActive(true);
+        Time.timeScale = 0f;
+        PauseMenu.gameIsPaused = true;
+    }
+
+
 }
