5df77ebfe75dc91ec3effc55c58826e5f0e4c777
akayy101 <ariba1kamran@gmail.com>
2024-08-08 16:44:02 +0300

    button sounds

---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSound : MonoBehaviour
+{
+    [SerializeField] private AudioClip buttonClickSound;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = buttonClickSound;
+    }
+
+    public void PlaySound()
+    {
+        audioSource.PlayOneShot(buttonClickSound);
+    }
+}
+
